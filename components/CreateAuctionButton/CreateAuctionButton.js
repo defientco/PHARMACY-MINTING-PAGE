c962bbfc3ce506ac40c32fd384308d93f3c8abbe
Sweets Sweetman <sweetmantech@gmail.com>
2022-11-08 00:26:03 -0300

    I add all contract interactions from module manager to createAuction.

--- components/CreateAuctionButton/CreateAuctionButton.js
+++ components/CreateAuctionButton/CreateAuctionButton.js
@@ -3,6 +3,7 @@ import { ethers } from 'ethers'
 import { toast } from 'react-toastify'
 import abi from '@lib/ERC721Drop-abi.json'
 import zoraModuleManagerAbi from '@lib/ZoraModuleManager-abi.json'
+import auctionAbi from '@lib/ReserveAuctionERC20FindersEth-abi.json'
 import { useAccount, useSigner } from 'wagmi'
 import { useConnectModal } from '@rainbow-me/rainbowkit'
 
@@ -18,19 +19,73 @@ const CreateAuctionButton = ({ contractAddress, tokenId }) => {
   const moduleManager = isEthereum
     ? '0x850A7c6fE2CF48eea1393554C8A3bA23f20CC401'
     : '0x9458E29713B98BF452ee9B2C099289f533A5F377'
+  const findersERC20 = isEthereum
+    ? '0xd1adAF05575295710dE1145c3c9427c364A70a7f'
+    : '0x36aB5200426715a9dD414513912970cb7d659b3C'
+  const chillToken = isEthereum
+    ? '0x67bD6541EB488059C8B84F859b0aD0244F9F0102'
+    : '0xc5Bc83bb51B41A83149A44DB03Eb86622a3A44b3'
 
   const getNftContract = () => {
     return new ethers.Contract(contractAddress, abi, signer)
   }
 
   const getModuleManagerContract = () => {
-    return new ethers.Contract(contractAddress, zoraModuleManagerAbi, signer)
+    return new ethers.Contract(moduleManager, zoraModuleManagerAbi, signer)
+  }
+
+  const getAuctionContract = () => {
+    return new ethers.Contract(findersERC20, auctionAbi, signer)
   }
 
   const isApprovedForAll = async (contract) => {
     const isApproved = await contract.isApprovedForAll(address, erc721TransferHelper)
     console.log('isApproved', isApproved)
     return isApproved
+  }
+
+  const isAuctionModuleApproved = async () => {
+    const contract = getModuleManagerContract()
+    let approved = await contract.isModuleApproved(address, findersERC20)
+
+    if (approved === false) {
+      try {
+        const tx = await contract.setApprovalForModule(findersERC20, true)
+        await tx.wait()
+        return true
+      } catch (err) {
+        console.error(err)
+        toast.error('error approving module manager for auction')
+      }
+    }
+    return approved
+  }
+
+  const createAuction = async () => {
+    try {
+      const contract = getAuctionContract()
+      const duration = 180
+      const reservePrice = 1
+      const sellerFundsRecipient = address
+      const startTime = 0
+      const bidCurrency = chillToken
+      const findersFeeBps = 808
+      const tx = await contract.createAuction(
+        contractAddress,
+        tokenId,
+        duration,
+        reservePrice,
+        sellerFundsRecipient,
+        startTime,
+        bidCurrency,
+        findersFeeBps
+      )
+      await tx.wait()
+      toast.success('auction created')
+    } catch (err) {
+      console.error(err)
+      toast.error('error creating auction')
+    }
   }
 
   const approveForAll = async (contract) => {
@@ -65,8 +120,10 @@ const CreateAuctionButton = ({ contractAddress, tokenId }) => {
       return
     }
 
-    const moduleManagerContract = getModuleManagerContract()
-    console.log('moduleManagerContract', moduleManagerContract)
+    const isModuleApproved = await isAuctionModuleApproved()
+    console.log('isModuleApproved', isModuleApproved)
+    if (!isModuleApproved) return
+    await createAuction()
   }
 
   return <Button onClick={handleClick}>Create Auction</Button>
