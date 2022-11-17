1179db382bf1abe9244d3cb77723f1d4bfff52d9
Sweets Sweetman <sweetmantech@gmail.com>
2022-11-17 19:56:32 -0300

    I remover unused imports.

--- components/Bid/Bid.tsx
+++ components/Bid/Bid.tsx
@@ -1,15 +1,3 @@
-import { Fragment, useState } from 'react'
-import { Listbox, Transition } from '@headlessui/react'
-import { SelectorIcon } from '@heroicons/react/solid'
-
-const sortOptions = [
-    { name: '1', queryValue: '1' },
-    { name: '2', queryValue: '2' },
-    { name: '3', queryValue: '3' },
-    { name: '4', queryValue: '4' },
-    { name: '5', queryValue: '5' }
-]
-
 export default function Bid({ colorScheme, initialBid, onChange }) {
     return (
         <div className={`text-[#f70500]  z-9 flex border border-red-500 col-start-1 col-end-3  w-full flex-row justify-self-end  focus-visible:ring-offset-[${colorScheme}`}>
